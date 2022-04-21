2a2f3e21cf32a8d2ea1f6206c53d3f8914aee44c
NitinSharma <NitinSharma@NitinSharma>
2022-04-22 00:27:29 +0530

    Added New test Cases

---
+++ End2EndSelenium_Jenkins/src/testCases/TC_005.java
@@ -0,0 +1,20 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_005 {
+	
+	@Test
+	public void tc_001() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Different");
+		driver.findElement(By.id("pass")).sendKeys("World");
+		driver.quit();
+		
+	}
+
+}
